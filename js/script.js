cf53c20c475bfe73025413d2af1a3f7c9730e22d
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-01-23 22:43:57 +0100

    add data

--- js/script.js
+++ js/script.js
@@ -2,5 +2,15 @@ console.log('Vue OK', Vue);
 
 // Attivo Vue
 const { createApp } = Vue;
-const app = createApp({});
+const app = createApp({
+    data: () => ({
+        name: 'ToDoList',
+        tasks: [
+            { id: 1, done: false, text: 'Fare la spesa' },
+            { id: 2, done: true, text: 'Lavare i capelli' },
+            { id: 3, done: false, text: 'Comprare una marca da bollo' },
+            { id: 4, done: false, text: 'Aggiornare il PC' }
+        ]
+    })
+});
 app.mount('#root');
