208eb3af08e99616960b88bc13ab1ca0dd48144b
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-01-23 23:27:00 +0100

    add function add task

--- js/script.js
+++ js/script.js
@@ -10,11 +10,22 @@ const app = createApp({
             { id: 2, done: true, text: 'Lavare i capelli' },
             { id: 3, done: false, text: 'Comprare una marca da bollo' },
             { id: 4, done: false, text: 'Aggiornare il PC' }
-        ]
+        ],
+        newTask: '',
     }),
     methods: {
         deleteTask(id) {
             this.tasks = this.tasks.filter(task => id !== task.id);
+        },
+        addTask() {
+            const newTask = {
+                id: new Date().toISOString(),
+                done: false,
+                text: this.newTask,
+            };
+
+            this.tasks.push(newTask);
+            this.newTask = '';
         }
     },
 });
